89945bde4e69ae6ddfd1173d12bd0a96379d0c06
MauroMarchesan88 <mauromarchesan88@gmail.com>
2025-09-10 22:14:19 -0300

    Adding base pygame funcs

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            screen.fill("black")
+            pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return        
 
 if __name__ == "__main__":
     main()
