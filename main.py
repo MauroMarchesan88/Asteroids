a55c56369a9430e547969cefca9a8452f524263f
MauroMarchesan88 <mauromarchesan88@gmail.com>
2025-09-10 22:30:51 -0300

    Adding first shapes and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +8,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
-            screen.fill("black")
-            pygame.display.flip()
             if event.type == pygame.QUIT:
-                return        
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        #FPS limit
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
